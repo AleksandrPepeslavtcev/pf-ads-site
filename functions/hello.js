5278787c6793809e1ae3a638cb22b607b384e4c1
AleksandrPepeslavtcev <aleksandr.pereslavtcev@outlook.com>
2025-10-16 23:06:08 +0300

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
 export async function onRequest() {
-  return new Response("hello from pages functions", {
+  return new Response("hello from pages functions WTF?", {
     headers: { "content-type": "text/plain; charset=utf-8" },
   });
 }
